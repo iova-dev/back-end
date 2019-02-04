ff13096a7652b193dfeefed10d8bcd6c12f43a50
Severiano Badajoz <sevebadajoz@gmail.com>
2019-02-04 11:59:41 -0800

    Create auth route

--- server/auth/auth.route.js
+++ server/auth/auth.route.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const controller = require('./auth.controller');
+
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-// #TODO: Implement authentication routes.
+router.route('/');
+
+router.post('/signup', (req, res) => {
+  controller.signUp(req.body)
+    .then((user) => {
+      controller.logUserIn(res, user);
+    }).catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+router.post('/login', (req, res) => {
+  controller.authUser(req.body.username, req.body.password)
+    .then((user) => {
+      controller.logUserIn(res, user);
+    }).catch((err) => {
+      res.status(401).send(err);
+    });
+});
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('lovaToken');
+  res.status(200).send();
+});
 
 module.exports = router;
