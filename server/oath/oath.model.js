f456832c63cbe1b213e5c2679b6af275ab191d98
tkrain23 <tk.rain09@gmail.com>
2019-02-12 23:09:35 -0800

    change filepath of oath model

---
+++ server/oath/oath.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const oathSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  money: {
+    type: Number,
+    required: true,
+  },
+  userID: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+});
+
+module.exports = mongoose.model('Oath', oathSchema);
