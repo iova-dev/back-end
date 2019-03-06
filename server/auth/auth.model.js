9783f64b36b906b8af7e2e8b51ac3cbd8a97df4c
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-06 01:21:51 -0800

    Reflect oath relationship in user schema

--- server/auth/auth.model.js
+++ server/auth/auth.model.js
@@ -11,7 +11,12 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true
-  }
+  },
+  oaths: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Oath',
+    default: [],
+  }]
 });
 
 userSchema.pre('save', function preSave(next) {
