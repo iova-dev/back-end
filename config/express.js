d92a5f1ac7663807cf75a2f32ab42990f1144540
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-06 02:38:46 -0800

    move middleware into express.js

--- config/express.js
+++ config/express.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('../index.route');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -10,6 +11,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Enable CORS - Cross Origin Resource Sharing.
 app.use(cors());
+
+// My auth middleware
+const checkAuth =(req, res, next) => {
+  if (typeof req.cookies.lovaToken === 'undefined' || req.cookies.lovaToken === null) {
+    console.log('no cookie');
+    // eslint-disable-next-line no-param-reassign
+    req.user = null;
+  } else {
+    console.log('found cookie');
+
+    const token = req.cookies.lovaToken;
+    const decodedToken = jwt.decode(token, {
+      complete: true
+    }) || {};
+    // eslint-disable-next-line no-param-reassign
+    req.user = decodedToken.payload;
+  }
+
+  next();
+};
+app.use(checkAuth);
 
 // Mount all routes on /api path.
 app.use('/api', routes);
